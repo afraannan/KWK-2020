829d534808b135fd26a3c8e76d40b90092798efa
afraannan <68658210+afraannan@users.noreply.github.com>
2020-07-22 21:03:12 -0400

    I finished actions and Outlets.

---
+++ Actions&Outlets/Actions&Outlets/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Actions&Outlets
+//
+//  Created by Apple on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    
+
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+            
+        }
+    }
+}
+
